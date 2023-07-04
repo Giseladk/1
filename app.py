8766829eed15ebb570163f0f6f418168e3a98493
Giseladk <137880764+Giseladk@users.noreply.github.com>
2023-07-04 20:33:26 +0700

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ col1.markdown(" Here is some info about me! ")
 image = Image.open("G.png")
 st.image(image, width=300)
 
-col3.metric(label="Welcome to", value="My Page" :tada: )
+col3.metric(label="Welcome to", value="My Page")
 
 st.markdown("## Profil", unsafe_allow_html=True)
 st.write("Nama :")
